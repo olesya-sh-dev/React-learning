ecee3c02d16a9ddd8ced784f60c2b16c1026585a
Olesya Shalay <olesyakublickaya@yandex.ru>
2024-04-01 11:14:52 +0300

    контролируемый input (checkbox, select)

--- src/stories/input.stories.tsx
+++ src/stories/input.stories.tsx
@@ -40,6 +40,39 @@ export const GetValueOfUncontrolledInputByButtonPress = () => {
   );
 };
 
+export const ControlledInput = () => {
+  const [parentValue, setParentValue] = useState("");
+  debugger;
+  const onChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setParentValue(e.currentTarget.value);
+  };
+  return <input value={parentValue} onChange={onChange} />;
+};
+
+export const ControlledCheckbox = () => {
+  const [parentValue, setParentValue] = useState(true);
+  debugger;
+  const onChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setParentValue(e.currentTarget.checked);
+  };
+  return <input type="checkbox" checked={parentValue} onChange={onChange} />;
+};
+export const ControlledSelect = () => {
+  const [parentValue, setParentValue] = useState<string | undefined>("2");
+  debugger;
+  const onChange = (e: ChangeEvent<HTMLSelectElement>) => {
+    setParentValue(e.currentTarget.value);
+  };
+  return (
+    <select value={parentValue} onChange={onChange}>
+      <option>none</option>
+      <option value="1">Minsk</option>
+      <option value="2">Moscow</option>
+      <option value="3">Kiev</option>
+    </select>
+  );
+};
+
 export const ControlledInputWithFixedValue = () => (
   <input value={"it-incubator.by"} />
 );
